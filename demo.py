b5a86af57d7d7a6309ba96709f447f9dcf942ee5
Alireza Aghamohammadi <aaghamohammadi@expedia.com>
2018-10-09 12:57:01 -0700

    simple python test example

---
+++ demo.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python
+import requests
+import json
+"""Simple class to demo used for test case"""
+class WorldData():
+    def __init__(self, path):
+        self.route = path
+
+    def write_result(self):
+        print(self.route)
+        return self.route  
+
+
+if __name__ == '__main__':
+
+    socialData = WorldData("happy") 
+    socialData.write_result()
---
+++ test.py
@@ -0,0 +1,23 @@
+import unittest, gc
+from demo import WorldData
+
+class WidgetTestCase(unittest.TestCase):
+    def setUp(self):
+        self.worldlData = WorldData('happy') 
+      
+
+    def tearDown(self):
+         self.worldlData = None
+         del self.worldlData
+         gc.collect()
+
+    def test_result_method(self):
+         self.assertEqual(self.worldlData.write_result(),'happy')
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
+
+
